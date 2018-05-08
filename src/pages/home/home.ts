105bb7994e6f098c41788914bd59975a83eae8e5
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-05-08 13:33:13 -0500

    Primera paguina

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,39 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  personajes = [];
   constructor(public navCtrl: NavController) {
-
+    this.personajes = [
+      {
+        'img': '../../assets/imgs/han-solo.jpg',
+        'nombre': 'Han Solo',
+        'titulo': 'Cazarrecompensas',
+        
+      },
+      {
+        'img': '../../assets/imgs/rey.jpg',
+        'nombre': 'Rey',
+        'titulo': 'Recolectora',
+        
+      },
+      {
+        'img': '../../assets/imgs/leia.jpg',
+        'nombre': 'Leia',
+        'titulo': 'Princesa',
+        
+      },
+      {
+        'img': '../../assets/imgs/finn.jpg',
+        'nombre': 'Finn',
+        'titulo': 'Piloto',
+        
+      },
+    ]
   }
 
+  call(img, nombre, titulo){
+    console.log("Hola");
+    console.log(nombre);
+    console.log(titulo);
+  }
 }
